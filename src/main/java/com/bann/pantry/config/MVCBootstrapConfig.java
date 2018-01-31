49268faff09925a16b8a3faa2fd92684ae266854
Mark Bann <markbann44@gmail.com>
2018-01-30 20:39:18 -0500

    first commit

---
+++ src/main/java/com/bann/pantry/config/MVCBootstrapConfig.java
@@ -0,0 +1,20 @@
+package com.bann.pantry.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "com.bann.pantry" })
+public class MVCBootstrapConfig  extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
+
